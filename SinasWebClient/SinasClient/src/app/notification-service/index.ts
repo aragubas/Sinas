a8a753afdb93a201600b0514d6513de300f8b650
Aragubas <dpaulootavio5@outlook.com>
2022-02-11 18:38:26 -0300

    g

--- SinasWebClient/SinasClient/src/app/notification-service/index.ts
+++ SinasWebClient/SinasClient/src/app/notification-service/index.ts
@@ -21,7 +21,7 @@ export class NotificationService
 
         if (notificationFound) { return; }
 
-        this.notifications.push(new Notification(content + Date.now()));
+        this.notifications.push(new Notification(content));
     }
 
 }
